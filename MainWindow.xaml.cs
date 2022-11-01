8576ea987db4ab82cb0a0b487f30f3db02dd9343
TehTemM@gmail.com <TehTemM@gmail.com>
2022-11-02 09:48:08 +1000

    Добавьте файлы проекта.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Wpf_Chemeris_PR5._1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void myGif_MediaEnded(object sender, RoutedEventArgs e)
+        {
+
+            myGif.Position = new TimeSpan(0, 0, 1);
+            myGif.Play();
+
+        }
+        private void myGif1_MediaEnded(object sender, RoutedEventArgs e)
+        {
+
+            myGif1.Position = new TimeSpan(0, 0, 1);
+            myGif1.Play();
+
+        }
+    }
+}
